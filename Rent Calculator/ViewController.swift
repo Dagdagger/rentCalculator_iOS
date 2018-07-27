7d68f8f1b743560880a868a4fd7b83ce11806333
Dagdagger <shelbyschwartz@yahoo.com>
2018-07-27 05:59:07 -0700

    Initial Commit

---
+++ Rent Calculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Rent Calculator
+//
+//  Created by Omar U. Brito on 7/27/18.
+//  Copyright © 2018 Omar U. Brito. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Rent CalculatorTests/Rent_CalculatorTests.swift
@@ -0,0 +1,36 @@
+//
+//  Rent_CalculatorTests.swift
+//  Rent CalculatorTests
+//
+//  Created by Omar U. Brito on 7/27/18.
+//  Copyright © 2018 Omar U. Brito. All rights reserved.
+//
+
+import XCTest
+@testable import Rent_Calculator
+
+class Rent_CalculatorTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
